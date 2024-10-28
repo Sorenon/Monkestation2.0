HolyTigerGod/Voidcrew

Breaks up the three quirk code files into individual quirks (#78407)
## About The Pull Request

Separates all the quirts out of the positive, neutral and negative files
into their own files. The smoker and alcoholic quirks are left with
junkie as they are subtypes of it.

## Why It's Good For The Game

These three code files were chonkers at 400, 483 and 1564 lines. They
were run on files of types only connected by all being quirks, and made
it hard to navigate these files without the user's eye sliding from one
quirk to the next.

Breaking them down makes it easy to look at the code for a specific
quirk indepentently, and also allows looking between multiple quirks
without having to scroll around the same file.

## Changelog

No player facing changes

code/datums/quirks/neutral_quirks/deviant_tastes.dm
@@ -1,0 +1,24 @@
+/datum/quirk/deviant_tastes
+	name = "Deviant Tastes"
+	desc = "You dislike food that most people enjoy, and find delicious what they don't."
+	icon = FA_ICON_GRIN_TONGUE_SQUINT
+	value = 0
+	gain_text = span_notice("You start craving something that tastes strange.")
+	lose_text = span_notice("You feel like eating normal food again.")
+	medical_record_text = "Patient demonstrates irregular nutrition preferences."
+	mail_goodies = list(/obj/item/food/urinalcake, /obj/item/food/badrecipe) // Mhhhmmm yummy
+
+/datum/quirk/deviant_tastes/add(client/client_source)
+	var/obj/item/organ/internal/tongue/tongue = quirk_holder.get_organ_slot(ORGAN_SLOT_TONGUE)
+	if(!tongue)
+		return
+	var/liked_foodtypes = tongue.liked_foodtypes
+	tongue.liked_foodtypes = tongue.disliked_foodtypes
+	tongue.disliked_foodtypes = liked_foodtypes
+
+/datum/quirk/deviant_tastes/remove()
+	var/obj/item/organ/internal/tongue/tongue = quirk_holder.get_organ_slot(ORGAN_SLOT_TONGUE)
+	if(!tongue)
+		return
+	tongue.liked_foodtypes = initial(tongue.liked_foodtypes)
+	tongue.disliked_foodtypes = initial(tongue.disliked_foodtypes)

code/datums/quirks/neutral_quirks/no_taste.dm
@@ -1,0 +1,10 @@
+/datum/quirk/no_taste
+	name = "Ageusia"
+	desc = "You can't taste anything! Toxic food will still poison you."
+	icon = FA_ICON_MEH_BLANK
+	value = 0
+	mob_trait = TRAIT_AGEUSIA
+	gain_text = span_notice("You can't taste anything!")
+	lose_text = span_notice("You can taste again!")
+	medical_record_text = "Patient suffers from ageusia and is incapable of tasting food or reagents."
+	mail_goodies = list(/obj/effect/spawner/random/food_or_drink/condiment) // but can you taste the salt? CAN YOU?!

Sorenon/Monkestation2.0

code/datums/quirks/neutral_quirks/deviant_tastes.dm
@@ -1,0 +1,24 @@
+/datum/quirk/deviant_tastes
+	name = "Deviant Tastes"
+	desc = "You dislike food that most people enjoy, and find delicious what they don't."
+	icon = FA_ICON_FACE_GRIN_TONGUE_SQUINT
+	value = 0
+	gain_text = span_notice("You start craving something that tastes strange.")
+	lose_text = span_notice("You feel like eating normal food again.")
+	medical_record_text = "Patient demonstrates irregular nutrition preferences."
+	mail_goodies = list(/obj/item/food/urinalcake, /obj/item/food/badrecipe) // Mhhhmmm yummy
+
+/datum/quirk/deviant_tastes/add(client/client_source)
+	var/obj/item/organ/internal/tongue/tongue = quirk_holder.get_organ_slot(ORGAN_SLOT_TONGUE)
+	if(!tongue)
+		return
+	var/liked_foodtypes = tongue.liked_foodtypes
+	tongue.liked_foodtypes = tongue.disliked_foodtypes
+	tongue.disliked_foodtypes = liked_foodtypes
+
+/datum/quirk/deviant_tastes/remove()
+	var/obj/item/organ/internal/tongue/tongue = quirk_holder.get_organ_slot(ORGAN_SLOT_TONGUE)
+	if(!tongue)
+		return
+	tongue.liked_foodtypes = initial(tongue.liked_foodtypes)
+	tongue.disliked_foodtypes = initial(tongue.disliked_foodtypes)

code/datums/quirks/neutral_quirks/no_taste.dm
@@ -1,0 +1,10 @@
+/datum/quirk/no_taste
+	name = "Ageusia"
+	desc = "You can't taste anything! Toxic food will still poison you."
+	icon = FA_ICON_FACE_MEH_BLANK
+	value = 0
+	mob_trait = TRAIT_AGEUSIA
+	gain_text = span_notice("You can't taste anything!")
+	lose_text = span_notice("You can taste again!")
+	medical_record_text = "Patient suffers from ageusia and is incapable of tasting food or reagents."
+	mail_goodies = list(/obj/effect/spawner/random/food_or_drink/condiment) // but can you taste the salt? CAN YOU?!